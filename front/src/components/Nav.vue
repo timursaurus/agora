804cd4da530d1949719ab6ef91e7d199669e2036
timursaurus <bolotovtmr@gmail.com>
2021-05-05 18:01:44 +0600

    smallest optimization possible lol

--- front/src/components/Nav.vue
+++ front/src/components/Nav.vue
@@ -50,7 +50,6 @@
                                          </p>
                                     </li>
                                 </ul>
-
                             </div>
                         </transition>
                     </div>
@@ -98,7 +97,17 @@ export default {
     },
     methods: {
         Search() {
-            if (this.search[0] == '@'){
+            if (this.search.length < 1) {
+                this.Defaults()
+            }
+            else if (this.search.length >= 1) {
+                HTTP.get('/api/room/?search=' + this.search)
+                .then((res) => {
+                    this.hints = []
+                    this.rooms = res.data
+                })
+            }
+            else if (this.search[0] == '@'){
                 if (this.search.length > 1) {
                     HTTP.get('/api/user/?search=' + this.search.slice(1))
                     .then((res) => {
@@ -119,18 +128,7 @@ export default {
                 } else {
                     this.categories = []
                 }
-            }
-            else if (this.search.length >= 1) {
-                HTTP.get('/api/room/?search=' + this.search)
-                .then((res) => {
-                    this.hints = []
-                    this.rooms = res.data
-                })
-                
-            }
-            else if (this.search.length < 1) {
-                this.Defaults()
-            }
+            }  
             
         },
         Defaults(){
